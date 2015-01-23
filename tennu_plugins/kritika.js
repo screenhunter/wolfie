ab02dc7eafd9eac2044a62a76ad259f6feb645ad
Rajat Khanna <rajatkhanna@Rajats-MacBook-Pro.local>
2015-01-22 22:57:16 -0500

    commit

--- tennu_plugins/kritika.js
+++ tennu_plugins/kritika.js
@@ -2,7 +2,7 @@ var KritikaPlugin = {
     init: function (client, imports) {
         return {
             handlers: {
-                '!!kritika': function (command) {
+                '!kritika': function (command) {
                     client.say(command.channel, 'Ciara/Arus/Falma/screenhunter has strong feelings for Kritika Singh <3<3');
                 }
             },
