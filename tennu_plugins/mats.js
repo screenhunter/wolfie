c5b9ca78e899b7a7e290121311b1f0c39e22b17b
Rajat Khanna <rajatkhanna@Rajats-MacBook-Pro.local>
2015-02-06 09:05:47 -0500

    commit

--- tennu_plugins/mats.js
+++ tennu_plugins/mats.js
@@ -57,6 +57,8 @@ var MATSPlugin = {
 
                                 Object.keys(loc).forEach(function(x) {
 
+                                    client.say(command.channel, x);
+
                                     if(x.toUpperCase() == string.toUpperCase())
                                         client.say(command.channel, "Locations found: " + inf[x]);
                                 });
